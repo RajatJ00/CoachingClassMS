445d743c5b5242dae03684cf9b230417ed848651
RajatJ00 <rajatjamgade00@gmail.com>
2023-11-04 19:17:02 +0530

    first commit

---
+++ AddBook.Designer.vb
@@ -0,0 +1,174 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AddBook
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button3 = New System.Windows.Forms.Button
+        Me.Panel1 = New System.Windows.Forms.Panel
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.txtBname = New System.Windows.Forms.TextBox
+        Me.Label8 = New System.Windows.Forms.Label
+        Me.txtBprice = New System.Windows.Forms.TextBox
+        Me.btnsave = New System.Windows.Forms.Button
+        Me.Label6 = New System.Windows.Forms.Label
+        Me.txtBqty = New System.Windows.Forms.TextBox
+        Me.Label7 = New System.Windows.Forms.Label
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Red
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.ForeColor = System.Drawing.Color.White
+        Me.Button3.Location = New System.Drawing.Point(1, 1)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(26, 22)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "X"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.DimGray
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.btnsave)
+        Me.Panel1.Controls.Add(Me.Label7)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.txtBqty)
+        Me.Panel1.Controls.Add(Me.txtBprice)
+        Me.Panel1.Controls.Add(Me.txtBname)
+        Me.Panel1.Location = New System.Drawing.Point(27, 51)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(735, 512)
+        Me.Panel1.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(167, 78)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(86, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "COURSE NAME"
+        '
+        'txtBname
+        '
+        Me.txtBname.BackColor = System.Drawing.Color.Silver
+        Me.txtBname.Font = New System.Drawing.Font("Microsoft Tai Le", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBname.Location = New System.Drawing.Point(356, 74)
+        Me.txtBname.Name = "txtBname"
+        Me.txtBname.Size = New System.Drawing.Size(217, 24)
+        Me.txtBname.TabIndex = 1
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Mistral", 15.75!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label8.Location = New System.Drawing.Point(562, 22)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(200, 26)
+        Me.Label8.TabIndex = 8
+        Me.Label8.Text = "ADD NEW COURSE DETAILS"
+        '
+        'txtBprice
+        '
+        Me.txtBprice.BackColor = System.Drawing.Color.Silver
+        Me.txtBprice.Font = New System.Drawing.Font("Microsoft Tai Le", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBprice.Location = New System.Drawing.Point(356, 197)
+        Me.txtBprice.Name = "txtBprice"
+        Me.txtBprice.Size = New System.Drawing.Size(133, 24)
+        Me.txtBprice.TabIndex = 5
+        '
+        'btnsave
+        '
+        Me.btnsave.BackColor = System.Drawing.Color.Black
+        Me.btnsave.ForeColor = System.Drawing.Color.White
+        Me.btnsave.Location = New System.Drawing.Point(196, 275)
+        Me.btnsave.Name = "btnsave"
+        Me.btnsave.Size = New System.Drawing.Size(210, 33)
+        Me.btnsave.TabIndex = 14
+        Me.btnsave.Text = "SAVE COURSE"
+        Me.btnsave.UseVisualStyleBackColor = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.ForeColor = System.Drawing.Color.White
+        Me.Label6.Location = New System.Drawing.Point(167, 145)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(64, 13)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "DURATION"
+        '
+        'txtBqty
+        '
+        Me.txtBqty.BackColor = System.Drawing.Color.Silver
+        Me.txtBqty.Font = New System.Drawing.Font("Microsoft Tai Le", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBqty.Location = New System.Drawing.Point(356, 141)
+        Me.txtBqty.Name = "txtBqty"
+        Me.txtBqty.Size = New System.Drawing.Size(133, 24)
+        Me.txtBqty.TabIndex = 6
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.ForeColor = System.Drawing.Color.White
+        Me.Label7.Location = New System.Drawing.Point(167, 197)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(34, 13)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "FEES"
+        '
+        'AddBook
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DimGray
+        Me.ClientSize = New System.Drawing.Size(786, 578)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Button3)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "AddBook"
+        Me.Opacity = 0.9
+        Me.Text = "AddBook"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtBname As System.Windows.Forms.TextBox
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents btnsave As System.Windows.Forms.Button
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents txtBprice As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents txtBqty As System.Windows.Forms.TextBox
+End Class
---
+++ AddBook.vb
@@ -0,0 +1,38 @@
+﻿Public Class AddBook
+
+    Private Sub AddBook_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.Top = 0
+        Me.Left = Mainform.Panel1.Width
+
+        Me.Width = Mainform.Width - Mainform.Panel1.Width
+        Me.Height = Mainform.Height
+
+        Panel1.Left = (Me.Width - Panel1.Width) / 2
+        Panel1.Top = (Me.Height - Panel1.Height) / 2
+
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Me.Close()
+    End Sub
+    Private Sub Emptyfields()
+        txtBname.Clear()
+        'txtBpublication.Clear()
+        txtBqty.Clear()
+        txtBprice.Clear()
+
+    End Sub
+    Private Sub btnsave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnsave.Click
+        If txtBname.Text <> "" And txtBqty.Text <> "" And txtBprice.Text <> "" Then
+            Module1.insert_update("insert into Course values('" & txtBname.Text & " ' , ' " & txtBqty.Text & " ' , ' " & txtBprice.Text & " ' )")
+            MsgBox(" Cousrse Added", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "Cousre Added")
+        Else
+            MsgBox("Fields Should Not Be Empty")
+        End If
+    End Sub
+
+    Private Sub Panel1_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+End Class
